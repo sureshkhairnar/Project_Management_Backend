8da5b18007697fe229a33600d517a66058caa4b9
suresh khairnar <suresh@mollatech.com>
2024-07-26 17:45:55 +0530

    schema defined for user and login api created

--- server.js
+++ server.js
@@ -5,13 +5,11 @@ const app = express();
 
 connectDB();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", require("./routes/auth"));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
