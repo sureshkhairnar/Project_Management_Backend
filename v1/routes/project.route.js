1236a0c8b2cf423b11470beab0f42d6e6caef16f
suresh khairnar <suresh@mollatech.com>
2024-08-05 10:53:50 +0530

    Project Management Assignment backend

---
+++ v1/routes/project.route.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/venues");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+const {
+  createProject,
+  updateProject,
+  deleteProject,
+  getSingleProject,
+  getAllProject,
+  projectStatistic,
+} = require("../controllers/project.controller");
+
+router.post("/", upload.array("images", 10), createProject);
+router.put("/:id", upload.array("images", 10), updateProject);
+router.delete("/:id", deleteProject);
+router.get("/:id", getSingleProject);
+router.get("/", getAllProject);
+router.get("/statistic", projectStatistic);
+
+module.exports = router;
